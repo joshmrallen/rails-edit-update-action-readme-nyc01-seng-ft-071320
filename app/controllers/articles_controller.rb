a643ccf306139bbbc1cea911c1aaf02e03228692
github username <joshmrallen@gmail.com>
2020-08-06 23:57:33 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update]
+  
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    find_article
   end
 
   def new
@@ -20,4 +22,24 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    find_article
+  end
+
+  def update
+    # raise params.inspect
+    find_article
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
 end
